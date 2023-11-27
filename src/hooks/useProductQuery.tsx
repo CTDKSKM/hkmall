7527ee0d76c025dfa0ee2b34a9b3b4efc0a6ece8
CTDKSKM <skmdldy001@gmail.com>
2023-11-27 17:35:59 +0900

    merge

--- src/hooks/useProductQuery.tsx
+++ src/hooks/useProductQuery.tsx
@@ -6,26 +6,19 @@ const QUERY_KEY = 'getAllProduct';
 const useProductQuery = () => {
   const query = useQueryClient();
 
-  const { isLoading, isError, data, error } = useQuery({
+  const { isPending, isError, data, error } = useQuery({
     queryKey: [QUERY_KEY],
     queryFn: getAllProductData
   });
 
-  //   const addProductMutation = useMutation({
-  //     mutationFn: addProduct,
-  //     onSuccess: () => {
-  //       query.invalidateQueries({ queryKey: [QUERY_KEY] });
-  //     }
-  //   });
   const deleteProductMutation = useMutation({
     mutationFn: deleteProduct,
     onSuccess: () => {
       query.invalidateQueries({ queryKey: [QUERY_KEY] });
     }
   });
-  if (isLoading) return { data: <div>로딩중입니다</div>, deleteProductMutation };
-  if (isError) return { data: <div>{error.message}</div>, deleteProductMutation };
-  return { data, deleteProductMutation };
+
+  return { isPending, isError, data, error, deleteProductMutation };
 };
 
 export default useProductQuery;
