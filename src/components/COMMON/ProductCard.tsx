a3806c80b3053d3b0d3bc8eb2f775caff0e58792
HojinLim <hojinim@gmail.com>
2023-12-04 10:15:30 +0900

    Design/메인페이지 카드 디자인

--- src/components/COMMON/ProductCard.tsx
+++ src/components/COMMON/ProductCard.tsx
@@ -13,17 +13,23 @@ const ProductCard = ({ item }: Props) => {
 
   return (
     <Link to={`/products/${item.id}`} className="productCardContainer">
-      <div className="overflow-hidden rounded-t-lg">
-        <img className="hover:scale-125 transition-transform duration-300" src={item.imgs[0]} alt="이미지없음" />
+      <div className="overflow-hidden rounded-t-lg items-center justify-center">
+        <img
+          className="hover:scale-125 transition-transform duration-300 w-[160px] h-[200px] p-3"
+          src={item.imgs[0]}
+          alt="이미지없음"
+        />
       </div>
-      <div className="p-6">
-        <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">{item.name}</h5>
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">{item.price}원</p>
-      </div>
-      <hr className="my-2" />
-      <div className="p-6 flex">
-        <AiFillHeart size={20} color="red" />
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">{item.like}</p>
+      <div className="me-9">
+        <div className="p-3 w-[150px] mx-3">
+          <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">{item.name}</h5>
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">{item.price}원</p>
+        </div>
+        {/* <hr className="my-2" /> */}
+        <div className="p-6 flex">
+          <AiFillHeart size={20} color="red" />
+          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">{item.like}</p>
+        </div>
       </div>
 
       {/* <button
--- src/components/SearchPage/SearchBox.tsx
+++ src/components/SearchPage/SearchBox.tsx
@@ -32,6 +32,7 @@ const SearchBox = (props: Props) => {
         value={searchValue}
         onChange={handleSearchInput}
         placeholder="검색어를 입력하세요"
+        maxLength={20}
       />
       <button onClick={handleSearchButton}>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32" fill="none">
